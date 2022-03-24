8d3f4ebb5f22f06cb616a78635e8a1700a50b015
aexol <aexol@aexol.com>
2022-03-24 12:18:09 +0100

    :sparkles: landing

--- packages/sandbox/src/layouts/layout.tsx
+++ packages/sandbox/src/layouts/layout.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { CustomHelmet } from '@/src/components';
+import { FiGithub } from 'react-icons/fi';
+import { Button, Link, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 
 export const siteTitle = 'Next.js Sample Website';
 
@@ -8,9 +11,26 @@ interface LayoutProps {
     pageTitle?: string;
 }
 
+const BB = styled(Button)`
+    position: absolute;
+    right: 10px;
+    top: 10px;
+    background: #666;
+    color: #fff;
+    :hover {
+        color: #666;
+    }
+`;
+
 export const Layout: React.FC<LayoutProps> = ({ children, home, pageTitle }) => {
     return (
         <div>
+            <Link href="https://github.com/aexol-studio/prosciutto-crudo">
+                <BB variant="solid">
+                    <Text mr="2">Github</Text>
+                    <FiGithub />
+                </BB>
+            </Link>
             <CustomHelmet pageTitle={pageTitle ? pageTitle : undefined} />
             {children}
         </div>
